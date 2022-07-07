acdbe544e096f9c29f5d2a0eeeb8a09a340d3280
Jihyun Ryu <77661701+ryuruz@users.noreply.github.com>
2022-07-07 16:40:43 +0900

    Update TypeConversion.java

--- 1. Basic/TypeConversion.java
+++ 1. Basic/TypeConversion.java
@@ -6,14 +6,16 @@ public class TypeConversion {
 		byte b=127;
 		int i=100;
 		
-		System.out.println(b+i);
-		System.out.println(10/4);
-		System.out.println(10.0/4);
-		System.out.println((char)0x12340041);
-		System.out.println((byte)(b+i));
-		System.out.println((int)2.9+1.8);
-		System.out.println((int)(2.9+1.8));
-		System.out.println((int)2.9 + (int)1.8);
+		System.out.println(b+i); //227
+		System.out.println(10/4); //2
+		System.out.println(10.0/4); //2.5
+		
+		System.out.println((char)0x12340041); // 체크
+		System.out.println((byte)(b+i)); // 체크
+		
+		System.out.println((int)2.9+1.8); // 3
+		System.out.println((int)(2.9+1.8)); // 4
+		System.out.println((int)2.9 + (int)1.8); //3
 		
 	}
 
